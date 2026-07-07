d78c09f117bcbe0b3acd5d79a77138bc8f225d8e
weihsun1128 <dt99039@dtes.chc.edu.tw>
2026-07-07 12:57:49 +0800

    表單送出會收到

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,41 @@
+    // 聯絡我
+    
+    // 抓輸入的值
+        function getValues(event) {
+
+        event.preventDefault();    
+
+        var name = document.getElementById("name");
+        var jobtitle = document.getElementById("jobtitle");
+        var phone = document.getElementById("phone");
+        var email = document.getElementById("email");
+        var subject = document.getElementById("subject");
+        var message = document.getElementById("message");
+
+        var rowData = {
+            name: name.value,
+            jobtitle: jobtitle.value,
+            phone: phone.value,
+            email: email.value,
+            subject: subject.value,
+            message: message.value,
+        };
+
+        fetch("https://script.google.com/macros/s/AKfycbziPPitUmJU0CXognzZ0HYxjuyF81p6s_OjFeBUeAtYGrNXwfleIAm8vTt_uL1Zm9C9/exec", {
+            method: "POST",
+            
+            body: JSON.stringify(rowData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("myForm").reset();
+            alert("謝謝您的訊息，會盡快處理您的留言！");
+        })
+        .catch(error => {
+            alert("送出失敗");
+            console.error(error);
+        });
+    }
 $(function (){
     
     //上方按鈕點選可跳至該位置 
@@ -127,4 +165,6 @@ $(function (){
     //         $(slides).css("transform", `translateX(-${index * 800}px)`);
     //     }
     // })
+
+
 })
